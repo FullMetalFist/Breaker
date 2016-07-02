4d73c20d80cedd0335c200ab173e5884b3f23ca0
Michael Vilabrera <m.vilabrera@gmail.com>
2016-07-02 17:13:49 -0400

    initial commit, end of chapter 6

---
+++ Breaker/GameViewController.swift
@@ -0,0 +1,58 @@
+//
+//  GameViewController.swift
+//  Breaker
+//
+//  Created by Michael Vilabrera on 6/27/16.
+//  Copyright (c) 2016 Michael Vilabrera. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+    
+    var scnView: SCNView!
+    var scnScene: SCNScene!
+    var game = GameHelper.sharedInstance
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 1
+        setupScene()
+        setupNodes()
+        setupSounds()
+    }
+    
+    // 2
+    func setupScene() {
+        scnView = self.view as! SCNView
+        scnView.delegate = self
+        scnScene = SCNScene(named: "Breaker.scnassets/Scenes/Game.scn")
+        scnView.scene = scnScene
+    }
+    
+    func setupNodes() {
+        
+    }
+    
+    func setupSounds() {
+        
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
+
+// 3
+extension GameViewController: SCNSceneRendererDelegate {
+    func renderer(renderer: SCNSceneRenderer, updateAtTime time: NSTimeInterval) {
+        
+    }
+}
